fec9f1311fcaa0f7a6bb9403ac51bf4c1db8b462
mobocop1988 <115904892+mobocop1988@users.noreply.github.com>
2026-09-02 13:40:35 -0500

    Create lists.js

---
+++ docs/lists.js
@@ -0,0 +1,41 @@
+/* ==========================================================================
+   MOBOCOP'S MASTER LIST OF LISTS - PERSISTENT DATA STORE
+   ========================================================================== */
+window.MOBOCOP_DATA = {
+  "background": "https://us-central1-menlo-view.menlosecurity.com/c/0/i/aHR0cHM6Ly93YWxscGFwZXJhY2Nlc3MuY29tL2Z1bGwvNTc2MTUzNC5qcGc~",
+  "lists": [
+    {
+      "subject": "Top 5 Video Games",
+      "items": [
+        "Diablo 2",
+        "Path of Exile 2",
+        "Teamfight Tactics",
+        "Ark: Survival Evolved",
+        "Baldur's Gate: Dark Alliance II"
+      ],
+      "dateAdded": "2 September 2026"
+    },
+    {
+      "subject": "Top 5 Movies",
+      "items": [
+        "Team America",
+        "Idiocracy",
+        "Avatar",
+        "Avatar 3",
+        "Avatar 2"
+      ],
+      "dateAdded": "1 September 2026"
+    },
+    {
+      "subject": "Top 5 Drinks",
+      "items": [
+        "Coffee",
+        "Sangria",
+        "Chocolate Milk",
+        "Beer",
+        "Root Beer"
+      ],
+      "dateAdded": "31 August 2026"
+    }
+  ]
+};
